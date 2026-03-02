cce6dcce226fd8360bc4779d7210076de64a4d5e
Varun J <vnjhaveri@gmail.com>
2026-03-02 15:59:08 +0530

    feat: new stuff
    feat: new stuff

--- calculator.c
+++ calculator.c
@@ -3,8 +3,8 @@
 int add(int a, int b) { return a + b; }
 int subtract(int a, int b) { return a - b; }
 int main() {
-  int x = 99, y = 88;
-  printf("Sum: %d\n", add(x, y));
-  printf("Subtraction: %d\n", subtract(x, y));
-  return 0;
+    int x = 99, y = 88;
+    printf("Sum: %d\n", add(x, y));
+    printf("Subtraction: %d\n", subtract(x, y));
+    return 0;
 }
