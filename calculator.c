46d495000e0fa5f4f7a081b980f9436acb4792f9
Madhura <madhura.lolayekar23@spit.ac.in>
2026-03-02 15:18:44 +0530

    Added subtraction feature

--- calculator.c
+++ calculator.c
@@ -3,9 +3,13 @@
 int add(int a, int b) {
     return a + b;
 }
-
+int subtract(int a, int b) {
+    return a - b;
+}
 int main() {
     int x = 10, y = 5;
     printf("Sum: %d\n", add(x, y));
+    printf("Subtraction: %d\n", subtract(x, y));
     return 0;
 }
+
